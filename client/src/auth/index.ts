74a523d787232f1dbce4ea834e27e71826889b56
Viviana Rosas <viviana.rosas@atomicobject.com>
2021-05-13 10:30:35 -0700

    stuff

--- client/src/auth/index.ts
+++ client/src/auth/index.ts
@@ -1,15 +1,32 @@
 import {
-  CognitoIdentityClient,
-  CreateIdentityPoolCommand,
-} from "@aws-sdk/client-cognito-identity";
+  CognitoIdentityProviderClient,
+  AddCustomAttributesCommand,
+  CognitoIdentityProviderClientConfig,
+  AdminCreateUserCommandInput,
+  AdminCreateUserCommand,
+  CognitoIdentityProvider,
+} from "@aws-sdk/client-cognito-identity-provider";
 
-const params = {
-  IdentityPoolName: "us-east-2_g2xFYohtO",
-  AllowUnauthenticatedIdentities: undefined,
+const params: CognitoIdentityProviderClientConfig = {
+  region: "us-west",
 };
 
-export const command = new CreateIdentityPoolCommand(params);
+const input: AdminCreateUserCommandInput = {
+  Username: "sillygirl",
+  UserPoolId: "us-west-1_MjG3zr8cy",
+  UserAttributes: [{ Name: "email", Value: "virosas@ucdavis.edu" }],
+};
+
+// const config: CognitoIdentityProviderClientConfig = {
+//   region: "us-west",
+//   credentials,
+// };
+
+const client = new CognitoIdentityProviderClient(params);
+// export const command = new AddCustomAttributesCommand(params);
+
+const command = new AdminCreateUserCommand(input);
+
+export const data = client.send(command);
 
 //https://nosugarpls/login?response_type=code&client_id=119b2dtmvsmhg7d2rhet74q32p&redirect_uri=http://localhost:3000
-
-export const cognitoClient = new CognitoIdentityClient({ region: "US-west" });
